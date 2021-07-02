4d89418bd316bc661889111f2d974b92a6d1fa82
Jon Keam <jpkeam@gmail.com>
2021-07-01 20:41:35 -0400

    add functional components

--- src/components/DropTokenSlot.js
+++ src/components/DropTokenSlot.js
@@ -1,19 +1,12 @@
 import React from 'react';
 
-export default class DropTokenSlot extends React.Component {
-    renderSlot(){
-        switch(this.props.identifier){
-            case 1:
-                return <div className="droptoken-player1 droptoken-slot"></div>
-            case 2:
-                return <div className="droptoken-player2 droptoken-slot"></div>
-            default:
-                return <div onClick={()=> this.props.dropper(this.props.coordinate)} className="droptoken-slot"></div>
-    
-        }
-    }
-
-    render(){
-        return this.renderSlot();
-    }
+export default function DropTokenSlot({ identifier, coordinate, dropper }) {
+  switch(identifier) {
+    case 1:
+      return <div className="droptoken-player1 droptoken-slot"></div>;
+    case 2:
+      return <div className="droptoken-player2 droptoken-slot"></div>;
+    default:
+      return <div onClick={()=> dropper(coordinate)} className="droptoken-slot"></div>;
+  }
 }
